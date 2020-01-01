ceb5a2d26c2a65704949d788f771f0a86b3d5718
Adison Lay <adisonlay@gmail.com>
2020-01-01 14:44:37 -0800

    Created tailwind.config.js to extend color palette, added hover color for skills icons

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'html': '#e54d26',
+        'css': '#3d8fc6',
+        'js': '#f0db4f',
+        'react': '#61dafb',
+        'php': '#6181b6',
+        'mysql': '#00618a',
+        'node': '#83CD29',
+        'bootstrap': '#59407f',
+        'jquery': '#0769ad',
+        'git': '#f34f29',
+        'github': '#181616',
+        'npm': '#cb3837',
+        'webpack': '#1C78C0',
+        'express': '#444',
+        'aws': '#F7A80D',
+        'ubuntu': '#DD4814',
+        'apache': '#303284'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
